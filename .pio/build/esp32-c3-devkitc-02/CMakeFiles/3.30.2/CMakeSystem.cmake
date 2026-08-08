fb0c2b35f2b7a2785147be9be6a0a8afff78c916
rosey <roseyanover@gmail.com>
2026-08-08 14:51:09 -0700

    restructured layout with all necessary files

---
+++ .pio/build/esp32-c3-devkitc-02/CMakeFiles/3.30.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26200")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Users/rosey/soundPainting/.pio/build/esp32-c3-devkitc-02/toolchain/toolchain-esp32c3.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
